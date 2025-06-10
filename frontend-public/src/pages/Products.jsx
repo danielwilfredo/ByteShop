124ab2556af184ced4bce30193392a9e8fbaddea
Daniel Wilfredo <xvwilito11@gmail.com>
2025-06-10 06:57:32 -0600

    moviendo logica de productos hacia custom hook

--- frontend-public/src/pages/Products.jsx
+++ frontend-public/src/pages/Products.jsx
@@ -1,68 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Title from "../components/Titles";
-import useFetchProducts from "../hooks/useFetchProducts";
 import CardProduct from "../components/Products/CardProduct";
 import { useParams } from "react-router-dom";
-
+import useDataProducts from "../components/Products/hooks/useDataProducts";
 const Products = () => {
-  const { products, categories } = useFetchProducts();
-  const [selectedCategory, setSelectedCategory] = useState("Todas");
-  const { id } = useParams();
-
-  // Cargar carrito del localStorage al iniciar
-  const [productsCart, setProductsCart] = useState(() => {
-    const savedCart = localStorage.getItem("cart");
-    return savedCart ? JSON.parse(savedCart) : [];
-  });
-
-  useEffect(() => {
-    if (id) {
-      setSelectedCategory(id);
-    } else {
-      setSelectedCategory("Todas");
-    }
-  }, [id]);
-
-  // Guardar carrito en localStorage cada vez que cambie
-  useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(productsCart));
-  }, [productsCart]);
-
-  const filteredProducts =
-    selectedCategory === "Todas"
-      ? products
-      : products?.filter((item) => item.idCategory?._id === selectedCategory);
-
-  // Función para agregar producto al carrito
-  const addToCart = (product, quantity) => {
-    setProductsCart((prevCart) => {
-      // Buscar si producto ya está en carrito
-      const existingProductIndex = prevCart.findIndex(
-        (p) => p.idProduct === product._id
-      );
-
-      if (existingProductIndex !== -1) {
-        // Actualizar cantidad y subtotal
-        const updatedCart = [...prevCart];
-        updatedCart[existingProductIndex].quantity += quantity;
-        updatedCart[existingProductIndex].subtotal =
-          updatedCart[existingProductIndex].quantity * product.price;
-        return updatedCart;
-      } else {
-        // Agregar nuevo producto
-        return [
-          ...prevCart,
-          {
-            idProduct: product._id,
-            quantity: quantity,
-            subtotal: product.price * quantity,
-          },
-        ];
-      }
-    });
-    console.log("Carrito actualizado:", productsCart);
-  };
-
+  const {
+    addToCart,
+    filteredProducts,
+    selectedCategory,
+    categories,
+    setSelectedCategory,
+    productsCart,
+    setProductsCart,
+  } = useDataProducts();
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <div className="max-w-6xl mx-auto bg-white shadow-md rounded-lg p-6">
